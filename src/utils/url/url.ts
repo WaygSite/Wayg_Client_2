104a3260a5e913db7a93ac0ff8ba6b10223bfa88
Thinker <102664109+YJH2848@users.noreply.github.com>
2023-05-16 17:13:49 +0900

    배포 오류

--- src/utils/url/url.ts
+++ src/utils/url/url.ts
@@ -1,5 +1,5 @@
 export const Site_Url = {
   MAIN_CLIENT: "/",
-  MAIN_SERVER: "https://master--marvelous-bunny-79d406.netlify.app/",
+  // MAIN_SERVER: "https://master--marvelous-bunny-79d406.netlify.app/",
   SEARCH: "searchPage/searchPage",
 };
