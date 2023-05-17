8b9ce488829d6b783ca97686430e8e404223d0b9
Thinker <102664109+YJH2848@users.noreply.github.com>
2023-05-17 19:59:20 +0900

    proxy 설정2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
 
 module.exports = {
-	async rewrites() {
-		return [
-			{
-				source: "/:path*",
-				destination: "http://localhost:3000/searchPage/searchPage",
-			},
-		];
-	},
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:5000/:path*",
+      },
+    ];
+  },
+};
